a6357d0447953f1fcd43e4510a7be5fbd038adfb
JesusCoyotzi <jesus_coyotzi@hotmail.com>
2015-06-15 19:56:25 -0500

    CursoC#
    Ejemplos para el curso C# básico SOMEFI

---
+++ Tipos/EntradaSalida/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaSalida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string saludo;
+            int i, j;
+            float f;
+            double d= 10.5;
+            bool b;
+            short s;
+             //Console.WriteLine("Hola, Ingresa un saludo por favor");
+            /*
+            saludo=Console.ReadLine();
+            Console.WriteLine(saludo);
+            */
+            Console.WriteLine("Hola, Ingresa un numero por favor");
+            i =Convert.ToInt32( Console.ReadLine());
+             f= Convert.ToSingle(Console.ReadLine());
+           s = Convert.ToInt16(Console.ReadLine());
+             i=(int)d;
+            Console.WriteLine("Ingresaste {0}, {1}, {2} ", i,f,s);
+            Console.WriteLine("El valor de double es {0} cateado a int es {1}", d, i);
+             Console.Read();
+            
+        }
+    }
+}
---
+++ Tipos/Tipos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int i=10,j=24,z=12;
+            Console.WriteLine("Mi variable i vale {0}", i);
+            Console.WriteLine("Un entero ocupa {0} bytes",sizeof(int));
+
+            Console.WriteLine("i= {0} z= {2} j= {1}", i , j , z);
+
+            Console.WriteLine("Un booleano ocupa {0} bytes", sizeof(bool));
+            Console.WriteLine("Un short ocupa {0} bytes", sizeof(short));
+            Console.WriteLine("Un long ocupa {0} bytes", sizeof(long));
+            Console.WriteLine("Un float ocupa {0} bytes", sizeof(float));
+            Console.WriteLine("Un double ocupa {0} bytes", sizeof(double));
+            Console.WriteLine("Un decimal ocupa {0} bytes", sizeof(decimal));
+
+            Console.Read();
+        }
+    }
+}
